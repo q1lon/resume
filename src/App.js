1796ccb63842479739b553c15d24bc83aaa952dd
q1lon <q1lon.zhang@gmail.com>
2020-08-19 17:51:27 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Layout, Card } from 'antd';
+const { Header, Footer, Sider, Content, Menu, Breadcrumb } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <Content style={{ padding: '0 50px' }}>
+            <div className="site-layout-content">Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>RESUME ©2020 Created by q1lon</Footer>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
