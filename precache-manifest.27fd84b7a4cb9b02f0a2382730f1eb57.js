50de17284392538c5e04f8f698f6fa70aa00ddcf
q1lon <q1lon@users.noreply.github.com>
2020-08-19 14:09:46 +0000

    deploy: 56032f1f50af2eccb2612c42ba5b92f9b9e397f1

---
+++ precache-manifest.27fd84b7a4cb9b02f0a2382730f1eb57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e55d64e03f6e05cf78ac0a993e7589",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90bba2fb7050db21d48d",
+    "url": "./static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "c8f4800fac232e96628d",
+    "url": "./static/css/main.be7b39e7.chunk.css"
+  },
+  {
+    "revision": "90bba2fb7050db21d48d",
+    "url": "./static/js/2.fa85545f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.fa85545f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f4800fac232e96628d",
+    "url": "./static/js/main.a3143f47.chunk.js"
+  },
+  {
+    "revision": "3d7194e73b6f06cf40b3",
+    "url": "./static/js/runtime-main.eaca50d7.js"
+  }
+]);
