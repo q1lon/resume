1aad3e6493a2f01936f9fbaa6f85ca71938faf5e
q1lon <q1lon.zhang@gmail.com>
2020-08-20 11:39:14 +0800

    添加模块分类

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
-import { Layout } from "antd";
+// import { Layout } from "antd";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="content">
-          <div style={{ width: "28%", backgroundColor: "#DDDDDD" }}>Sider</div>
-          <div style={{ width: "72%", backgroundColor: "#fff" }}>Content</div>
-        </div>
+          <div className="sider">Sider</div>
+          <div className="content">Content</div>
       </div>
     );
   }
