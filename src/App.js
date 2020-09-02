a69d97bff59531437b4604fd894361567a528c78
q1lon <q1lon.zhang@gmail.com>
2020-09-03 00:16:53 +0800

    添加高德地图

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 // import { Layout } from "antd";
+import { Map } from 'react-amap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <div className="sider">Sider</div>
-          <div className="content">Content</div>
+          <div className="content"><Map amapkey="a04e4904244e1ccb614d350ca68d312c" /></div>
       </div>
     );
   }
